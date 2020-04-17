20f3074f4c404e0383a1ede581ddffaa0789155f
Hadaiq Ahmad <14bscshahmad@seecs.edu.pk>
2020-04-17 16:20:47 +0500

    Updates

---
+++ precache-manifest.d26e2e7464ac2642de14f00d7b56eb74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d861428a81dda27b3b29294a870a2fcb",
+    "url": "/coronalive/index.html"
+  },
+  {
+    "revision": "025451540ba0c9000a90",
+    "url": "/coronalive/static/css/main.a33a7dc6.chunk.css"
+  },
+  {
+    "revision": "89fe7e11c4d90af924a8",
+    "url": "/coronalive/static/js/2.d0e69bcc.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/coronalive/static/js/2.d0e69bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "025451540ba0c9000a90",
+    "url": "/coronalive/static/js/main.6d1c21cc.chunk.js"
+  },
+  {
+    "revision": "c58a13a3c585e5e85e97",
+    "url": "/coronalive/static/js/runtime-main.c9c005d8.js"
+  }
+]);
